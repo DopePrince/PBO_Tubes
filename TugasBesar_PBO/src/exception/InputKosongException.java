4e12da19a47d833755d6aa4e577ff4c17aeff355
DopePrince <theys100@gmail.com>
2022-06-03 13:44:11 +0700

    InputKosongException

---
+++ TugasBesar_PBO/src/exception/InputKosongException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package exception;
+
+/**
+ * Nama  : Kenneth Vincentius Theys
+ * NPM   : 200710681
+ * Kelas : PBO - B
+**/
+public class InputKosongException extends Exception{
+    public String message(){
+        return "Field input tidak boleh kosong!";
+    }
+}
