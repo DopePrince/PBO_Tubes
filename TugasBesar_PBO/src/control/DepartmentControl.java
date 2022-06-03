e02d946774fbaed7c9ff4634c2073e13ae9e3b35
DopePrince <theys100@gmail.com>
2022-06-03 13:31:41 +0700

    Membuat folder control

---
+++ TugasBesar_PBO/src/control/DepartmentControl.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package control;
+
+/**
+ * Nama  : Kenneth Vincentius Theys
+ * NPM   : 200710681
+ * Kelas : PBO - B
+**/
+public class DepartmentControl {
+
+}
