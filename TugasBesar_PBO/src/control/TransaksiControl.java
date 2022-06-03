a92d5a8b55b709e38dbe6221b4d4f86c50459de7
DopePrince <theys100@gmail.com>
2022-06-03 13:52:11 +0700

    Transaksi belum lengkap

--- TugasBesar_PBO/src/control/TransaksiControl.java
+++ TugasBesar_PBO/src/control/TransaksiControl.java
@@ -5,11 +5,36 @@
 
 package control;
 
+import dao.TransaksiDAO;
+import java.util.List;
+import model.Transaksi;
+
 /**
  * Nama  : Kenneth Vincentius Theys
  * NPM   : 200710681
  * Kelas : PBO - B
 **/
 public class TransaksiControl {
-
+    private TransaksiDAO dDao = new TransaksiDAO();
+    
+    public void insertDataTransaksi(Transaksi tr) {
+        dDao.insertTransaksi(tr);
+    }
+    
+//    public TableTransaksi showDataTransaksi(String query){
+//        
+//        List<Transaksi> dataTransaksi = dDao.showTransaksi(query);
+//        TableTransaksi tableTransaksi = new TableTransaksi(dataTransaksi);
+//        
+//        return tableTransaksi;
+//    }
+    
+    public void updateDataTransaksi(Transaksi tr){
+        dDao.updateTransaksi(tr);
+    }
+    
+    public void deleteDataTransaksi(int id){
+        dDao.deleteTransaksi(id);
+    }
+    
 }
