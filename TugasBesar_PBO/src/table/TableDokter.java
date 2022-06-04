b48ca4722e438fdd87f34d4f1a77e1f76537ccb1
DopePrince <theys100@gmail.com>
2022-06-04 15:05:41 +0700

    Folder table

---
+++ TugasBesar_PBO/src/table/TableDokter.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package table;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import model.Dokter;
+
+/**
+ * Nama  : Kenneth Vincentius Theys
+ * NPM   : 200710681
+ * Kelas : PBO - B
+**/
+public class TableDokter extends AbstractTableModel{
+
+}
---
+++ TugasBesar_PBO/src/table/TablePasien.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package table;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import model.Pasien;
+
+/**
+ * Nama  : Kenneth Vincentius Theys
+ * NPM   : 200710681
+ * Kelas : PBO - B
+**/
+public class TablePasien extends AbstractTableModel{
+
+}
