7c56e82819f4d4898bc363ed1067a4eca5cb7af3
DopePrince <theys100@gmail.com>
2022-06-04 15:07:23 +0700

    Comment

--- TugasBesar_PBO/src/dao/RuanganDAO.java
+++ TugasBesar_PBO/src/dao/RuanganDAO.java
@@ -6,10 +6,14 @@
 package dao;
 
 /**
- * Nama  : Kenneth Vincentius Theys
- * NPM   : 200710681
- * Kelas : PBO - B
+ * Kelompok 5 :
+ * 200710534 - Nicholas Suharto
+ * 200710587 - Aldyo Putra
+ * 200710607 - Henry
+ * 200710653 - Tia Vianka Yustin
+ * 200710681 - Kenneth Vincentius Theys
 **/
+
 public class RuanganDAO {
 
 }
