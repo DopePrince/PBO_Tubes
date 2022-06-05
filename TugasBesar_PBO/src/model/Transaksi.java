304449101ca2a8a56e300c3b000758c37546a584
Henry03 <henryyanggg@gmail.com>
2022-06-05 14:28:12 +0700

    Update Transaksi.java
    nambah biaya diagnosis

--- TugasBesar_PBO/src/model/Transaksi.java
+++ TugasBesar_PBO/src/model/Transaksi.java
@@ -16,13 +16,15 @@ package model;
 
 public class Transaksi {
     private int id;
+    private double biaya_diagnosis;
     private String tanggal_transaksi;
     private Pasien pasien;
     private Dokter dokter;
     private Ruangan ruangan;
 
-    public Transaksi(int id, String tanggal_transaksi, Pasien pasien, Dokter dokter, Ruangan ruangan) {
+    public Transaksi(int id, double biaya_diagnosis, String tanggal_transaksi, Pasien pasien, Dokter dokter, Ruangan ruangan) {
         this.id = id;
+        this.biaya_diagnosis = biaya_diagnosis;
         this.tanggal_transaksi = tanggal_transaksi;
         this.pasien = pasien;
         this.dokter = dokter;
@@ -37,6 +39,16 @@ public class Transaksi {
         this.id = id;
     }
 
+    public double getBiaya_diagnosis() {
+        return biaya_diagnosis;
+    }
+
+    public void setBiaya_diagnosis(double biaya_diagnosis) {
+        this.biaya_diagnosis = biaya_diagnosis;
+    }
+    
+    
+    
     public String getTanggal_transaksi() {
         return tanggal_transaksi;
     }
