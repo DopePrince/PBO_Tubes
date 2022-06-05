3a9b0701089fff52987cbeeab690b6d7bf364105
Henry03 <henryyanggg@gmail.com>
2022-06-05 11:04:11 +0700

    Update Pasien.java
    Nambah jenis penyakit dan ruangan

--- TugasBesar_PBO/src/model/Pasien.java
+++ TugasBesar_PBO/src/model/Pasien.java
@@ -21,24 +21,18 @@ public class Pasien {
     private String jenis_kelamin;
     private String alamat;
     private String no_telepon;
-<<<<<<< Updated upstream
-    private String jenis_pennyakit;
+    private Ruangan ruangan;
+    private Jenis_Penyakit penyakit;
 
-    public Pasien(String id, int umur, String nama, String gender, String alamat, String no_telepon, String jenis_pennyakit) {
-=======
-    private String no_ruangan;
-    private String jenis_penyakit;
-    
-    public Pasien(String id, int umur, String nama, String jenis_kelamin, String alamat, String no_telepon, String no_ruangan, String jenis_penyakit, Dokter dokter) {
->>>>>>> Stashed changes
+    public Pasien(String id, int umur, String nama, String jenis_kelamin, String alamat, String no_telepon, Ruangan ruangan, Jenis_Penyakit penyakit) {
         this.id = id;
         this.umur = umur;
         this.nama = nama;
         this.jenis_kelamin = jenis_kelamin;
         this.alamat = alamat;
         this.no_telepon = no_telepon;
-        this.no_ruangan = no_ruangan;
-        this.jenis_penyakit = jenis_penyakit;
+        this.ruangan = ruangan;
+        this.penyakit = penyakit;
     }
 
     public String getId() {
@@ -89,24 +83,22 @@ public class Pasien {
         this.no_telepon = no_telepon;
     }
 
-    
-    public String getNo_ruangan(){
-        return no_ruangan;
-    }
-    
-    public void setNo_ruangan(String no_ruangan){
-        this.no_ruangan = no_ruangan;
-    }
-    
-    
-    
-    public String getJenis_penyakit() {
-        return jenis_penyakit;
+    public Ruangan getRuangan() {
+        return ruangan;
     }
 
-    public void setJenis_pennyakit(String jenis_penyakit) {
-        this.jenis_penyakit = jenis_penyakit;
+    public void setRuangan(Ruangan ruangan) {
+        this.ruangan = ruangan;
     }
+
+    public Jenis_Penyakit getPenyakit() {
+        return penyakit;
+    }
+
+    public void setPenyakit(Jenis_Penyakit penyakit) {
+        this.penyakit = penyakit;
+    }
+
     
  
 }
